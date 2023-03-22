ec42722ca0d91004c5b13cc0d2a21c1b0e11ef75
Matheus Assis <assis.matheus1@gmail.com>
2023-03-22 18:17:12 -0300

    add | adding new html layout

--- love_map/static/main.js
+++ love_map/static/main.js
@@ -1,9 +1,15 @@
 var map = L.map('mapid').setView([-23.5505, -46.6333], 13);
 var mmIcon = L.icon({
-    iconUrl: "../images/mm_logo.png",
-    iconSize: [75, 30],
-    iconAchor: [37.5, 15],
-    popupAnchor: [0, 0]
+    iconUrl: "../images/mm_logo_round.png",
+    iconSize: [75, 37.5],
+    iconAchor: [37.5, 17.75],
+    popupAnchor: [10, 10]
+})
+var foodIcon = L.icon({
+    iconUrl: "../images/food_logo_round.png",
+    iconSize: [60, 60],
+    iconAchor: [30, 30],
+    popupAnchor: [10, 10]
 })
 
 
@@ -11,20 +17,43 @@ L.tileLayer("https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=nC
     maxZoom: 18,
 }).addTo(map);
 
-fetch("https://ourlovemap-api.onrender.com/api/date_markers")
+// fetch("https://ourlovemap-api.onrender.com/api/date_markers")
+//   .then(response => response.json())
+//   .then(markers => {
+//     markers.forEach(marker => {
+//       var m = L.marker(marker.location, {icon: mmIcon}).addTo(map);
+//       m.bindPopup(`<h1>${marker.title}</h1><h4>${marker.subtitle}</h4><p>${marker.description}</p><img src='${marker.image}' width=100% height=100%/>`).openPopup();
+//     });
+//   });
+
+fetch("http://127.0.0.1:5000/api/food_markers")
+  .then(response => response.json())
+  .then(markers => {
+    markers.forEach(marker => {
+      var m = L.marker(marker.location, {icon: foodIcon}).addTo(map);
+      m.bindPopup(
+        `
+          <h2>${marker.title}</h2>
+          <h3>${marker.subtitle}</h3>
+          <p>${marker.description}</p>
+          <img src='${marker.image}' width=100% height=100%/>
+        `
+      ).openPopup();
+    });
+  });
+
+fetch("http://127.0.0.1:5000/api/date_markers")
   .then(response => response.json())
   .then(markers => {
     markers.forEach(marker => {
       var m = L.marker(marker.location, {icon: mmIcon}).addTo(map);
-      m.bindPopup(`<h1>${marker.title}</h1><h4>${marker.subtitle}</h4><p>${marker.description}</p><img src='../images/mm_logo.png' width=75px height=30px/>`).openPopup();
+      m.bindPopup(
+        `
+          <h2>${marker.title}</h2>
+          <h3>${marker.subtitle}</h3>
+          <p>${marker.description}</p>
+          <img src='${marker.image}' width=100% height=100%/>
+        `
+      ).openPopup();
     });
   });
-
-  // fetch("http://127.0.0.1:5000/api/date_markers")
-  // .then(response => response.json())
-  // .then(markers => {
-  //   markers.forEach(marker => {
-  //     var m = L.marker(marker.location, {icon: mmIcon}).addTo(map);
-  //     m.bindPopup(`<h1>${marker.title}</h1><h3>${marker.subtitle}</h3><p>${marker.description}</p><img src='../images/mm_logo.png' width=100% height=100%/>`).openPopup();
-  //   });
-  // });
